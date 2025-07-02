5be33ee35cc225b608fb60c122b9a5b9fd89af96
Rick <rickpsayland@gmail.com>
2025-07-02 07:12:02 -0400

    pull endpoint out into own file

---
+++ PersonalProjectsAPI/Endpoints/WeatherForecastEndpoints.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace PersonalProjectsAPI;
+
+public static class WeatherForecastEndpoints
+{
+    public static void MapWeatherForecastEndpoints(this IEndpointRouteBuilder app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", 
+            "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    }).ToArray();
+
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+    }
+}
